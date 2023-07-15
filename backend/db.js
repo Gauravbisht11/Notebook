5de73ba614320436bc42d50b39a6667a367a030c
Gauravbisht11 <bgaurav.8911@gmail.com>
2023-07-15 13:47:11 +0530

    vercel commit

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
-const mongooseURI="mongodb://127.0.0.1:27017"
+const mongooseURI="mongodb+srv://Gauravbisht:fnIG92q3Tzbp0IWz@cluster0.y1hmiwd.mongodb.net/?retryWrites=true&w=majority"
 
+// mongodb://127.0.0.1:27017
 const connectToMongo=()=>{
     mongoose.connect(mongooseURI)
     // console.log('hello')
