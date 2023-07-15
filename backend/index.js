7ecfc6ad1ddb4ec7275f06521e7504aadc2ce9e0
Gauravbisht11 <bgaurav.8911@gmail.com>
2023-07-15 16:28:20 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,6 @@ app.use(cors(
     origin:["https://mern-notebook-brown.vercel.app"],
     methods:["POST","GET"],
     credentials:true,
-    headers: [
-      { "key": "Access-Control-Allow-Credentials", "value": "true" },
-      { "key": "Access-Control-Allow-Origin", "value": "*" },
-      { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-      { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-    ]
   }
 ))
 const port = 5000
