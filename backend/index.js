44075feb730d6e89ff8466f0f67bc15be313fa6c
Gauravbisht11 <bgaurav.8911@gmail.com>
2023-07-15 16:15:12 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ connectToMongo()
 const app = express()
 app.use(cors(
   {
-    origin:[],
+    origin:["https://mern-notebook-brown.vercel.app"],
     methods:["POST","GET"],
     credentials:true
   }
